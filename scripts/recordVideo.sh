a04a573cb290a7644711717959ad2272c9bf3778
Alex Smith <smitaw@musc.edu>
2022-06-03 12:13:24 -0400

    Updated recordVideo.sh

--- scripts/recordVideo.sh
+++ scripts/recordVideo.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+#This script records a video and copies it to network location defined in .bashrc (REMOTEVIDPATH)
+#Arguments: $1:Name of Video, $2: Length (minutes), $3: Frames per second
 ans=$(echo "$2 * 60000" | bc)
 now=$(date "+%F")
-name=$(echo $HOSTNAME)
-raspivid -t $ans -br 50 -w 600 -h 800 --framerate $3 -b 10000000 -awb greyworld -o $1_$name_$now.h264
-MP4Box -add $1_$name_$now.h264 $1_$name_$now.mp4 -fps $3
-sshpass -f ".pass_file" scp $1_$name_$now.mp4 rpi@10.81.104.166:/d1/studies/Raspicam/
-rm $1_$name_$now.h264
+echo "Started recording at" $(date +%H:%M:%S)
+raspivid -t $ans -br 50 -w 600 -h 800 --framerate $3 -b 10000000 --awb greyworld -o $1"_"$HOSTNAME"_"$now.h264
+echo "Converting to mp4"
+MP4Box -add $1"_"$HOSTNAME"_"$now.h264 $1"_"$HOSTNAME"_"$now.mp4 -fps $3
+echo "Started data transfer to " $REMOTEVIDPATH " at " $(date +%H:%M:%S)
+sshpass -p $REMOTEPASS scp $1"_"$HOSTNAME"_"$now.mp4 $REMOTE:$REMOTEVIDPATH
+echo "Finished data transfer at " $(date)
+rm $1"_"$HOSTNAME"_"$now.*
+echo "Finished for now..."
