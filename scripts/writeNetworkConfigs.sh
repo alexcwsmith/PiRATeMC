399a8fc1a571f2a927d602192377791125c19653
Alex Smith <smitaw@musc.edu>
2022-11-30 13:30:38 -0500

    Fixed typo

--- scripts/writeNetworkConfigs.sh
+++ scripts/writeNetworkConfigs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir ~/Desktop/networkBackups/
-echo "Created folder at $HOME/Desktop/networkBacksup/ and writing files there"
-$(echo "hostname -I") > ~/Desktop/networkBackups/IPaddr.conf
-ip route | grep default | tee ~/Desktop/networkBackups/ip_route.conf
-ip a s | grep -e “<” grep -e “inet” | tee ~/Desktop/networkBackups/ip_a_s.conf
-cat /etc/resolv.conf | grep nameserver | tee ~/Desktop/networkBackups/resolvconf.conf
-cp /etc/netplan/*.yaml ~/Desktop/networkBackups/
-cp /etc/dhcp/dhcpd.conf ~/Desktop/networkBackups/
-cp /etc/default/isc-dhcp-server ~/Desktop/networkBackups/
-cp /etc/default/bind9 ~/Desktop/networkBackups/
+[ -d "$HOME/Desktop/networkBackups/" ] && echo "networkBackups folder already found on desktop, overwriting contents"
+[ ! -d "$HOME/Desktop/networkBackups/" ] && mkdir $HOME/Desktop/networkBackups/
+$(echo "hostname -I") > $HOME/Desktop/networkBackups/IPaddr.conf
+ip route | grep default | tee $HOME/Desktop/networkBackups/ip_route.conf
+ip a s | grep -e "<" -e "inet" | tee $HOME/Desktop/networkBackups/ip_a_s.conf
+cat /etc/resolv.conf | grep nameserver | tee $HOME/Desktop/networkBackups/resolvconf.conf
+cp /etc/netplan/*.yaml $HOME/Desktop/networkBackups/
+cp /etc/dhcp/dhcpd.conf $HOME/Desktop/networkBackups/
+cp /etc/default/isc-dhcp-server $HOME/Desktop/networkBackups/
+cp /etc/default/bind9 $HOME/Desktop/networkBackups/
 exit
