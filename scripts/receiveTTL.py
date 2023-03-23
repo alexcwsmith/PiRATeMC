1fdc07e0c437f671f9f809263e19df6fa46aa673
Alex Smith <smitaw@musc.edu>
2023-03-23 11:08:10 -0400

    Minor fixes

--- scripts/receiveTTL.py
+++ scripts/receiveTTL.py
@@ -4,11 +4,19 @@
 Created on Fri Feb 11 12:10:03 2022
 
 This script requires setting Linux environment variables with the name, duration (in minutes), and frame rate.
-To do this, before running this script either run the below commands in a terminal (using your own desired values), or add these to your .bashrc file if they will not change (video names will have a date && Pi ID to appended them).
+To do this, before running this script either run the below commands in a terminal (using your own desired values), 
+or add these to your .bashrc file if they will not change (video names will have a date && Pi ID to appended them).
 
 export vidName='NameOfVideo'
 export vidLength=60
 export vidFPS=30
+
+If you are unsure if you are exporting variables correctly, after you run the export command above 
+you can see the variable in the terminal using the 'echo' command and putting a $ in front of the variable name,
+for example putting this into the terminal should print 60:
+echo $vidName
+
+ 
 
 @author: James M. Welsh & Alexander Smith
 """
@@ -20,10 +28,18 @@ import os
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
-
-vidName = os.environ['vidName']
-vidLength = os.environ['vidLength'
-vidFPS = os.environ['vidFPS']
+try:
+    vidName=os.environ['vidName']
+except KeyError:
+    print("No environment variable set for vidName, create one by exporting one in a terminal, for example:\n export vidName='exampleVideoName'")
+try:
+    vidLength = os.environ['vidLength']
+except KeyError:
+    print("No environment variable set for vidLength, create one by exporting one in a terminal, for example:\n export vidLength=60")
+try:
+    vidFPS = os.environ['vidFPS']
+except KeyError:
+    print("No environment variable set for vidFPS, create one by exporting one in a terminal, for example:\n export vidFPS=60")
 
 print("Ready to receive TTL")
 while True:
