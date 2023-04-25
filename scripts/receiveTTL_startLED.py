d12764eb70d80b86d41e440b0a9bb92acdc64511
Alex Smith <smitaw@musc.edu>
2023-04-25 14:18:22 -0400

    Added logging

--- scripts/receiveTTL_startLED.py
+++ scripts/receiveTTL_startLED.py
@@ -7,10 +7,14 @@ Created on Tue Apr 25 11:39:03 2023
 """
 
 import RPi.GPIO as GPIO
-from time import sleep
-import subprocess
-import os
-from datetime import datetime as dt
+import logging
+import time
+
+logging.basicConfig(filename=time.strftime('%m-%d-%Y'),
+                    filemode='a',
+                    format='%(asctime)s:%(msecs)d, %(name)s, %(levelname)s, %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
@@ -18,9 +22,10 @@ GPIO.setup(17, GPIO.OUT)
 
 def callback_on(self):
     GPIO.output(17, 1)
+    logging.info('START TTL, LED ON')
 
 def callback_off(self):
-    GPIO.output(17, 0)
+    GPIO.output(17, 0, 'END TTL, LED OFF')
 
 GPIO.add_event_detect(4, GPIO.FALLING, bouncetime=50)
 GPIO.add_event_callback(4, callback_on)
