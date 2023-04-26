8e8764c12a1a24e2616952e2a1af9850e7ddbeeb
Alex Smith <smitaw@musc.edu>
2023-04-26 19:40:13 -0400

    Edited to match order in paper

--- networking/writeNetworkConfigs.sh
+++ networking/writeNetworkConfigs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 [ -d "$HOME/Desktop/networkBackups/" ] && echo "networkBackups folder already found on desktop, overwriting contents"
 [ ! -d "$HOME/Desktop/networkBackups/" ] && mkdir $HOME/Desktop/networkBackups/
+ip a s | grep -e "<" -e "inet" | tee $HOME/Desktop/networkBackups/ip_a_s.conf
 $(echo "hostname -I") > $HOME/Desktop/networkBackups/IPaddr.conf
 ip route | grep default | tee $HOME/Desktop/networkBackups/ip_route.conf
-ip a s | grep -e "<" -e "inet" | tee $HOME/Desktop/networkBackups/ip_a_s.conf
 cat /etc/resolv.conf | grep nameserver | tee $HOME/Desktop/networkBackups/resolvconf.conf
 cp /etc/netplan/*.yaml $HOME/Desktop/networkBackups/
 cp /etc/dhcp/dhcpd.conf $HOME/Desktop/networkBackups/
