e8a88fb1247d6e2b2c5bcc9ffc073d8711d1d2e8
Alex Smith <smitaw@musc.edu>
2023-05-02 12:17:41 -0400

    Added detail to comments

--- AnalyzeVideos_DLC_Conda.sh
+++ AnalyzeVideos_DLC_Conda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#initialize anaconda:
+#initialize anaconda (copy this from your .bashrc file):
 __conda_setup="$('/home/smith/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -12,8 +12,14 @@ else
     fi
 fi
 unset __conda_setup
+
+
 conda init bash
+
 #activate the environment and run script:
+
+#PLACE THE NAME OF YOUR CONDA ENV ON LINE BELOW:
 conda activate dlc
 python3 /d1/software/PiRATeMC/AnalyzeVideos_DLC.py
+
 exit
